4bce49fc20a77bc06d047e15b1c43777649d5407
Vivek Makwana <vivekmakwana654gmail.com>
2024-02-03 13:41:20 +0530

    model training added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from CDClassification import logger
 from CDClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CDClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from CDClassification.pipeline.stage_03_training import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -21,6 +22,18 @@ try:
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n-----------------------------------")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"********************")
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<\n\n------------------------------")
 except Exception as e:
         logger.exception(e)
         raise e
